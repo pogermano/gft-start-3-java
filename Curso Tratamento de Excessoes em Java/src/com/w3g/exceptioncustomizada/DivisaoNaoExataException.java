36f3eb1b271987f9748a27cc2218d6e35a524a54
pogermano <pogermano@gmail.com>
2022-01-17 15:07:28 -0300

    Exemplo ExceptionCustomizada2

---
+++ Curso Tratamento de Excessoes em Java/src/com/w3g/exceptioncustomizada/DivisaoNaoExataException.java
@@ -0,0 +1,12 @@
+package com.w3g.exceptioncustomizada;
+
+public class DivisaoNaoExataException extends Exception{
+    private int numerado;
+    private int denominador;
+
+    public DivisaoNaoExataException(String message, int numerado, int denominador) {
+        super(message);
+        this.numerado = numerado;
+        this.denominador = denominador;
+    }
+}
