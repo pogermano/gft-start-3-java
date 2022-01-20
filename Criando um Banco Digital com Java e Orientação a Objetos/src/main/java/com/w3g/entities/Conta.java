f195cff01e07ea8cacf346b8d7a9a06fa01a5173
pogermano <pogermano@gmail.com>
2022-01-19 23:53:53 -0300

    Polimorfismo

--- Criando um Banco Digital com Java e Orientação a Objetos/src/main/java/com/w3g/entities/Conta.java
+++ Criando um Banco Digital com Java e Orientação a Objetos/src/main/java/com/w3g/entities/Conta.java
@@ -1,9 +1,18 @@
 package com.w3g.entities;
 
 public abstract class Conta implements  IConta {
-    private int agencia;
-    private int numero;
-    private int saldo;
+
+    private final static int AGENCIA_PADRAO = 1;
+    private static int SEQUENCIAL = 1;
+
+    protected int agencia;
+    protected int numero;
+    protected int saldo;
+
+    public Conta() {
+        this.agencia = AGENCIA_PADRAO;
+        this.numero = SEQUENCIAL++;
+    }
 
     @Override
     public void sacar(double valor) {
--- Criando um Banco Digital com Java e Orientação a Objetos/src/main/java/com/w3g/entities/IConta.java
+++ Criando um Banco Digital com Java e Orientação a Objetos/src/main/java/com/w3g/entities/IConta.java
@@ -1,7 +1,9 @@
 package com.w3g.entities;
 
 public interface IConta {
-     void sacar(double valor);
+
+
+    void sacar(double valor);
      void depositar(double valor);
      void tranferir(Conta contaDestino, double valor);
 }
