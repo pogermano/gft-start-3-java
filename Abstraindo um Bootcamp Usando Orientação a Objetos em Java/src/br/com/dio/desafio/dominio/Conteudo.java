01aac76da1a4d3e8438a54c6df619c23d6219167
pogermano <pogermano@gmail.com>
2022-01-20 13:40:05 -0300

    Herança e polimorfismo

---
+++ Abstraindo um Bootcamp Usando Orientação a Objetos em Java/src/br/com/dio/desafio/dominio/Conteudo.java
@@ -0,0 +1,31 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+
+public abstract class Conteudo {
+    protected static final double XP_PADRAO = 10d;
+
+    private String titulo;
+    private String descricao;
+
+
+
+    protected abstract double calcularXp();
+
+
+    protected String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
--- Abstraindo um Bootcamp Usando Orientação a Objetos em Java/src/br/com/dio/desafio/dominio/Mentoria.java
+++ Abstraindo um Bootcamp Usando Orientação a Objetos em Java/src/br/com/dio/desafio/dominio/Mentoria.java
@@ -2,29 +2,11 @@ package br.com.dio.desafio.dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria {
-    private String titulo;
-    private String descricao;
+public class Mentoria  extends Conteudo{
     private LocalDate data;
 
 
     public Mentoria() {
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
     }
 
     public LocalDate getData() {
@@ -35,12 +17,18 @@ public class Mentoria {
         this.data = data;
     }
 
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO + 20;
+    }
+
     @Override
     public String toString() {
         return "Mentoria{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
-                ", data=" + data +
+                "titulo=" + getTitulo() +
+                "descrição=" + getDescricao() +
+                "data=" + data +
                 '}';
     }
 }
