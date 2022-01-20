19dc5d3d9ceffc5598b35742393407b438bf870b
pogermano <pogermano@gmail.com>
2022-01-20 12:59:29 -0300

    Mentoria e Curso

---
+++ Abstraindo um Bootcamp Usando Orientação a Objetos em Java/src/Main.java
@@ -0,0 +1,30 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso java");
+        curso1.setDescricao("Descrição curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso js");
+        curso2.setDescricao("Descrição curso js");
+        curso2.setCargaHoraria(8);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria Java");
+        mentoria1.setDescricao("Descrição mentoria Java");
+        mentoria1.setData(LocalDate.now());
+
+
+
+
+
+        System.out.println(curso1);
+        System.out.println(mentoria1);
+    }
+}
