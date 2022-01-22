a67d5612c71eeaddcf9fc3478bf7dd7977b397af
pogermano <pogermano@gmail.com>
2022-01-22 12:52:45 -0300

    Projeto final

---
+++ Explorando Padroes de Projetos na Pratica com Java/lab-padroes-projeto-spring/src/main/java/com/w3g/labpadroesprojetospring/gof/model/ClienteRepository.java
@@ -0,0 +1,9 @@
+package com.w3g.labpadroesprojetospring.gof.model;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends CrudRepository<Cliente, Long> {
+
+}
---
+++ Explorando Padroes de Projetos na Pratica com Java/lab-padroes-projeto-spring/src/test/java/com/w3g/labpadroesprojetospring/gof/LabPadroesProjetoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w3g.labpadroesprojetospring.gof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabPadroesProjetoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
